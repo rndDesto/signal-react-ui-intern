3952cd0d84d0f98e9b9b1a07a42fb92702734ffe
mzikrillahihsn <mzikrillahihsan@gmail.com>
2024-01-04 15:24:35 +0700

    signal button baru

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,9 +3,9 @@ import styles from './style.module.css'
 const Button = () => {
   return (
     <div className={[
-      styles['button-saya'],
-      styles['mantul-gan']
-    ].join(" ")}>Button</div>
+      styles['button-content'],
+      styles['button-border']
+    ].join(" ")}>signal button</div>
   )
 }
 
