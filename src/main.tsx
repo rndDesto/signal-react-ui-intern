5905ab092a5775343a85ee86a664c49437214795
DESKTOP-5282VE2\destotalago <widdesto.yudistiro@gmail.com>
2024-01-08 10:33:36 +0700

    home desto

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Example from './Pages/Example/Example.tsx';
 import Home from './Pages/Home/Home.tsx';
+import HomeDesto from './Pages/DestoTsel/Home/Home.tsx';
 import { Stepper } from './Components/Stepper/Stepper.tsx';
 import Button from './Components/Zikri/Button/Button.tsx';
 
@@ -29,7 +30,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/desto",
-    element: <Home />,
+    element: <HomeDesto />,
   },
 ]);
 
