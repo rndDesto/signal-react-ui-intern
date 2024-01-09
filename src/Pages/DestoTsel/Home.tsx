299cb92e465dba483307ae0acc3b0dd054993dac
TELKOMSEL\WiddestoYud <widdesto_a_yudistiro@telkomsel.co.id>
2024-01-09 11:13:46 +0700

    tambah label

--- src/Pages/DestoTsel/Home.tsx
+++ src/Pages/DestoTsel/Home.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
+import SignalButton from '../../Components/Button/Button'
 import Button from '../../Components/Desto/Button/Button'
+import { Images } from '../../Components/Desto/Images/Images'
 import Label from '../../Components/Desto/Label/Label'
 
 const Home = () => {
+  const [warna, setWarna] = useState('')
   return (
     <div>
         <h1>Home Desto</h1>
+        <p onClick={()=>setWarna('primary')}>warna merah</p>
+        <p onClick={()=>setWarna('secondary')}>warna kuning</p>
+        <p onClick={()=>setWarna('tritary')}>warna hijau</p>
         <Button />
-        <Label />
+        
+        <Label rounded="2" bg={warna}>
+          <Images w="30" height="30" src="https://cdn-icons-png.flaticon.com/128/665/665049.png" />
+          <p>data tidak ditemukan</p>
+          <SignalButton namaButton="oke, mengerti" warna="info" />
+        </Label>
     </div>
   )
 }
