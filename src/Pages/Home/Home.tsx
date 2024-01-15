6348c27cfff6162562ebae4c97c65aecde6b0cea
mzikrillahihsn <mzikrillahihsan@gmail.com>
2024-01-15 11:54:29 +0700

    fix signal button

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1>Pesanan anda</h1>
       <img src="https://api.duniagames.co.id/api/product/upload/image/20832643771638334759.jpg" />
       <SignalButton namaButton="Pesan Barang" warna="primary" size="small" full="false" disabled="false" variant="2" />
-      <SignalButton namaButton="Kembali" warna="warning" size="larger" full="false" disabled="false" variant="2" />
+      <SignalButton namaButton="Kembali" warna="warning" size="larger" full="false" disabled="false" variant="1" />
       <SignalButton namaButton="Pesan Lagi" warna="secondary" size="medium" full="false" disabled="false" variant="2" />
       
   
