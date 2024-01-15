a19f7273c207a4f3958fd325e7171e2d066124be
AgilFaturrahman <faturrahmanagil1905@gmail.com>
2024-01-15 11:47:27 +0700

    fix signal callout

--- src/Components/Agil/Images/Images.tsx
+++ src/Components/Agil/Images/Images.tsx
@@ -3,8 +3,8 @@ import styles from './style.module.css'
 export const Images = (props) => {
     const {src, w, h} = props
   return (
-    <div className={styles.images}>
-        <img src={src} width={w} height={h} />
+    <div className="flex">
+        <img src={src}  />
     </div>
   )
 }
