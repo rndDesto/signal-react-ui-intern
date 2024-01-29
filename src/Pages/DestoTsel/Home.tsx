b332474fcfb6687bf3b3f070cb498f45797da244
@nadhifa08 <nadhifa0803@gmail.com>
2024-01-29 12:17:22 +0700

    MiniApp Nadhifa

--- src/Pages/DestoTsel/Home.tsx
+++ src/Pages/DestoTsel/Home.tsx
@@ -592,6 +592,7 @@ const Home = () => {
       }
     ]
   }
+  
 
 
   return (
--- src/Pages/Nadhifafa/MiniApp.tsx
+++ src/Pages/Nadhifafa/MiniApp.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SignalChips from '../../Components/Nadhifa/Chips/Chips';
 import SignalSnackBar from '../../Components/Nadhifa/SnackBar/signalSnackBar';
 import SignalButton from '../../Components/Zikri/Button/Signal-Button'
 import SignalBreadcrumb from '../../Components/Zikri/Breadcrumb/SignalBreadcrumb'
 import SignalCallout from '../../Components/Agil/Callout/signalCallout';
 import SignalCard from '../../Components/Agil/Card/SignalCard';
+import SignalBody from "../../Components/Agil/Body/SignalBody"
 
 const MiniApp = () => {
     const dataPasien = {
@@ -87,106 +88,173 @@ const MiniApp = () => {
         ]
       }
       //Callout
-      const [calloutOpen, setCalloutOpen] = useState(true);
-      const breadcrumbItems = [
+      
+      
+      const [Data, setData] = useState(dataPasien.listDokter);
+      const [snackBarVisible, setSnackBarVisible] = useState(false);
+      const [selectedJadwal, setSelectedJadwal] = useState(null);
+      const [selectedDokterIndex, setSelectedDokterIndex] = useState(null);
+      const [buttonDisabledState, setButtonDisabledState] = useState(
+        dataPasien.listDokter.map((_, index) => index !== selectedDokterIndex)
+      );
+      const [selectedPoli, setSelectedPoli] = useState(null);
+      
+      const [calloutOpen] = useState(true);
+      const breadcrumbItems = selectedJadwal
+      ? [
         { name: 'Home', href: '/' },
-        { name: 'Button', href: '/button' },
-        { name: 'Tab', href: '/tab' },
-        { name: 'Typography', href: '/typography' },
-        { name: 'Education', href: '/education' },
-        { name: 'Address', href: '/address' }
+        { name: selectedJadwal.nama},
+        { name: selectedJadwal.poli },
+        { name: selectedJadwal.jadwal },   
       ]
-      //Chips
-      const [data, setData] = useState(dataPasien.listDokter);
-      const handleChipClick = (jadwalIndex: number) => {
-        setData((prevState) =>
-          prevState.map((jadwal, i) => ({
-            ...jadwal,
-            isDisabled: i === jadwalIndex,
-          }))
+      :[{ name: "Home", href: "/agil" }]
+
+      const handleChipClick = (filterpasienIndex:any, jadwalIndex:any) => {
+        setData((prevPasien) =>
+          prevPasien.map((pasien, i) => {
+            if (i === filterpasienIndex) {
+              const updatedJadwaldokter = pasien.jadwal.map((jadwaldokter, idx) => ({
+                ...jadwaldokter,
+                isSelected: idx === jadwalIndex,
+              }));
+              
+              const selectedJadwalPraktekDetails ={
+                nama: pasien.nama,
+                poli: pasien.poli,
+                jadwal: updatedJadwaldokter [jadwalIndex].name,
+              }
+
+              console.log("selectedJadwalPraktekDetails")
+              setSelectedJadwal(selectedJadwalPraktekDetails);
+
+              const isAtLeastOneSelected = updatedJadwaldokter.some(
+                (jadwaldokter) => jadwaldokter.isSelected
+              )
+    
+              // Update state hanya untuk film tertentu
+              setButtonDisabledState((prev) =>
+                prev.map((prevItem, prevIndex) =>
+                  prevIndex === filterpasienIndex ? !isAtLeastOneSelected : prevItem
+                )
+              );
+
+              setSelectedDokterIndex(filterpasienIndex)
+              return {
+                ...pasien,
+                jadwal: updatedJadwaldokter,
+              };
+            }else{
+              return {
+                ...pasien,
+                jadwal:pasien.jadwal.map((jadwaldokter) =>({
+                  ...jadwaldokter,
+                  isSelected:false,
+                })),
+              };
+            }
+          })
+        );
+        setButtonDisabledState(() =>
+        dataPasien.listDokter.map((_, index) => index !== filterpasienIndex)
         );
       };
-      const jaya=dataPasien.listDokter
-      console.log(dataPasien)
-      console.log('jaya=',jaya)
-      console.log('perbandingan',dataPasien.listDokter===jaya)
 
-      // const [position, setPosition] = useState('')
-      // const [color, setColor] = useState('');
-    
+      const filteredPraktek = selectedPoli
+      ? Data.filter((pasien) => pasien.poli === selectedPoli)
+      :Data;
+      
+      // console.log(SignalButton);
+      // setSelectedPoli(null)
+      
+      const handleButtonClick = () => {
+        setSnackBarVisible(true);
+      };
+      
+
+
   return (
-    <>
-    {/* <SignalSnackBar position={position}>
-            <div className="flex justify-between m-2">
-              <div className="grow">can't show this page</div>
-              <div className="grow-0">Retry</div>
-            </div>
-          </SignalSnackBar>   */}
-        
-    {/* <div>Nama Pasien = {dataPasien.namaPasien}</div> */}
-    {calloutOpen && (
-        <SignalCallout color="info"  openCallout={calloutOpen}>
-          <div>
-            <h3>Nama :{dataPasien.namaPasien}</h3>
-            <p>NIK: {dataPasien.nik}</p>
-            <p>Telepon: {dataPasien.telp}</p>
+    <>    
+    <div>
+        <SignalCallout color="info" openCallout={calloutOpen}>
+          <div className="flex flex-col flex-1 ml-2">
+            <SignalBody>Nama: {dataPasien.namaPasien}</SignalBody>
+            <SignalBody>NIK: {dataPasien.nik}</SignalBody>
+            <SignalBody>Telepon: {dataPasien.telp}</SignalBody>
           </div>
         </SignalCallout>
-      )}
-      <div className="space-x-4 p-3 ">
-      <SignalBreadcrumb items={breadcrumbItems}/>
       </div>
 
-      <SignalCard>
-        {dataPasien.listDokter.map((dokter, index) => (
-          <div className="">
-          <div key={index} className="border-solid border border-gray-200 mb-5 bg-white-50 mx-12 rounded-sm w-auto">
-              {/* left */}
-              <div className="flex">
-              <div className="">
-                <img src={dokter.photo} alt={dokter.nama} className=" py-5  mx-7 rounded-sm" />
-                
-                  <div className= " mx-7 text-left font-normal text-gray-800 ">
-                    <p>{dokter.nama}</p>
-                    <p>Poli: {dokter.poli}</p>
-                  </div>   
-              </div> 
-              
-                {/* center */}
-            <div className="py-16 pl-52">
-              <div className="text-center font-bold">
-                <p>{dokter.rumah_sakit}</p>
-              </div>     
-              <div className="flex">
-                {dokter.jadwal.map((jadwal, jadwalIndex) => (
-                  <div className="px-10 py-4">
+      <div className="space-x-4 p-3 ">
+        <SignalBreadcrumb items={breadcrumbItems}></SignalBreadcrumb>
+      </div>
+
+      <div className="flex flex-auto py-1"> 
+      {Array.from(new Set(dataPasien.listDokter.map((pasien) => pasien.poli))).map(
+        (poli, index) => (
+          <SignalChips
+          key={index}
+          data={{name:poli}}
+          onClick={() => setSelectedPoli(poli)}
+          img=""/>
+        )
+      )}
+      </div>
+
+      {filteredPraktek.map((pasien, pasienIndex) => (
+        <div key={pasienIndex} className=" border border-gray-300 max-w-[1200px] m-auto p-5 rounded-md mb-5">
+          <SignalCard>
+          <div className="flex gap-2 items-center">
+            <div>
+              <div>
+                <img src={pasien.photo} alt={pasien.nama} />
+              </div>
+              <div>
+                  <SignalBody>{pasien.nama}</SignalBody>
+                  <SignalBody>Poli: {pasien.poli}</SignalBody>
+              </div>
+            </div>
+            <div className="grow">
+            <p className='text-center text-lg font-bold'>{pasien.rumah_sakit}</p>
+                <p className='text-center text-sm mb-5'>Jadwal Praktek</p>
+              <div className="flex gap-2 flex-wrap justify-evenly">
+                {pasien.jadwal.map((jadwaldokter, jadwalIndex) => (
                   <SignalChips
                     key={jadwalIndex}
-                    data={jadwal}
-                    onClick={() => handleChipClick(jadwalIndex)} 
-                    img ='' 
-                    />
-                  </div>
-                    ))}
-                  
-              </div> 
-            </div>
-            
-              {/* right */}
-              <div className="ml-auto my-24">
-                <SignalButton namaButton="Pesan" >bsbjabjb</SignalButton>
+                    data={jadwaldokter}
+                    onClick={() => handleChipClick(pasienIndex, jadwalIndex)}
+                    img=""
+                  />
+                ))}
               </div>
-            </div> 
             </div>
-          </div>    
-          ))}
-        </SignalCard>
-    
-    
+
+            <div className="ml-auto my-24 w-[130px]">
+              <SignalButton
+                variant="1"
+                color="info"
+                size="large"
+                full={true}
+                disable={buttonDisabledState[pasienIndex]}
+                onClick={handleButtonClick}
+                namaButton="Pesan"
+              ></SignalButton>
+            </div>
+          </div>
+          </SignalCard> 
+        </div>
+      ))}
+      {snackBarVisible && (
+      <SignalSnackBar sticky="false" position="topCenter">
+        <div className="flex justify-between m-2">
+          <div className="grow">Telah berhasil memesan jadwal kunjungan</div>
+          <div className="grow-0" onClick={() => setSnackBarVisible(false)}>
+            Ulangi
+          </div>
+        </div>
+      </SignalSnackBar>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default MiniApp
-      
-          
